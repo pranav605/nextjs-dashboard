5e70a49a69dc68d3ad75a2cbf2a0ac68ec4fe1f8
pranav605 <pranav605@gmail.com>
2025-01-18 18:35:34 -0500

    Fixed build issues

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -61,6 +61,8 @@ export async function createInvoice(prevState: State, formData: FormData) {
     `;
     } catch (error) {
         // If a database error occurs, return a more specific error.
+        console.log(error);
+        
         return {
             message: 'Database Error: Failed to Create Invoice.',
         };
@@ -100,6 +102,8 @@ export async function updateInvoice(
         WHERE id = ${id}
       `;
     } catch (error) {
+        console.log(error);
+        
         return { message: 'Database Error: Failed to Update Invoice.' };
     }
 
--- app/ui/invoices/edit-form.tsx
+++ app/ui/invoices/edit-form.tsx
@@ -27,6 +27,7 @@ export default function EditInvoiceForm({
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
         {/* Customer Name */}
         <div className="mb-4">
+          <p className='hidden'>{JSON.stringify(state)}</p>
           <label htmlFor="customer" className="mb-2 block text-sm font-medium">
             Choose customer
           </label>
